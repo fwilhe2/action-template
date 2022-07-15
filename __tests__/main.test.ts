5c0eb5a7d1f292d154312a60287c626382869565
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-07-15 12:01:34 +0200

    Add test from actions/typescript-action template

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,24 @@
 import {greeting} from '../src/main'
+import * as process from 'process'
+import * as cp from 'child_process'
+import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
 test('dummy test', async () => {
   const foo = await greeting('Max')
   await expect(foo).toEqual('Hello, Max!')
+})
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', async () => {
+  process.env['INPUT_GUEST_NAME'] = 'Michael'
+  const nodePath = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env
+  }
+
+  const stdout = cp.execFileSync(nodePath, [ip], options).toString()
+
+  await expect(stdout).toStrictEqual('Hello, Michael!\n')
 })
